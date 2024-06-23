a94a8b250e387cfa284b5c4cd4837714481acf6f
battle_angel <six@angel>
2024-06-23 01:56:56 -0400

    update process & get time

--- ac3.cpp
+++ ac3.cpp
@@ -6,6 +6,10 @@ ac3::ac3(QWidget *parent)
     , ui(new Ui::ac3)
 {
     ui->setupUi(this);
+    connect(ui->sliderA, &QSlider::sliderMoved, this, &ac3::attach_process);
+
+    update_console(get_time( ) + " AC trainer by SIX © ");
+
 }
 
 ac3::~ac3()
@@ -13,7 +17,21 @@ ac3::~ac3()
     delete ui;
 }
 
-void ac3::open_process( )
+void ac3::attach_process( )
 {
 
+}
+
+void ac3::update_console( QString msg )
+{
+    csl_msg += msg + "\n";
+
+    ui->console_LBL->setText(csl_msg);
+}
+
+QString ac3::get_time ( )
+{
+    auto now = std::chrono::system_clock::now( );
+    std::time_t cut = std::chrono::system_clock::to_time_t(now);
+    return QString::fromStdString(std::ctime(&cut));
 }
--- ac3.h
+++ ac3.h
@@ -2,6 +2,11 @@
 #define AC3_H
 
 #include <QMainWindow>
+#include <QSlider>
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,10 +22,15 @@ public:
     ac3(QWidget *parent = nullptr);
     ~ac3();
 
+public slots:
+    void attach_process ( );
+
 protected:
-    void open_process( );
+    void update_console ( QString msg );
+    QString get_time    ( );
 
 private:
     Ui::ac3 *ui;
+    QString csl_msg = ""; // console message
 };
 #endif // AC3_H
